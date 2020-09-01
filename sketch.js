94327c761a9264fdb77ae0335bf6a147bf86925f
Jiao Wang <jiao228228@gmail.com>
2020-09-01 20:30:45 +0200

    add jumping sound

--- sketch.js
+++ sketch.js
@@ -1,11 +1,7 @@
 /*
-
-- Copy your game project code into this file
 - for the p5.Sound library look here https://p5js.org/reference/#/libraries/p5.sound
 - for finding cool sounds perhaps look here
 https://freesound.org/
-
-
 */
 var gameChar_x;
 var gameChar_y;
@@ -34,8 +30,6 @@ var jumpSound;
 function preload()
 {
     soundFormats('mp3','wav');
-    
-    //load your sounds here
     jumpSound = loadSound('assets/jump.wav');
     jumpSound.setVolume(0.1);
 }
